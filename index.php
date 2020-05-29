bbebde79b06d8c9fc662f97b0c3f3df5ea332604
Evan Hildreth <evan.hildreth@webdevstudios.com>
2020-05-29 11:38:02 -0400

    Create empty Index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Silence is golden, but these are the words that the world needs to hear. -DC Talk, "Say the Words"
+ *
+ * @since 0.1.0
+ * @package Smolblog\Social
+ */
+
+// Do nothing.
